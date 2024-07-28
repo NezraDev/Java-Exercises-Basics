e892500ed9aa7212f1a3b17a45d2dfbe0a510036
NezraDev <nezraprogrammer@gmail.com>
2024-07-29 00:37:09 +0800

    finalized the calculator

--- Simple Mortage Calculator/src/App.java
+++ Simple Mortage Calculator/src/App.java
@@ -1,5 +1,28 @@
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        long principal = scanner.nextLong();
+      
+        System.out.print("Annual Interest Rate: ");
+        double annual = scanner.nextDouble() / 100 / 12;
+       
+
+        System.out.print("Period (Years): ");
+        int years = scanner.nextInt() * 12;
+
+        double answer = principal * (annual * Math.pow(1 + annual, years)) / (Math.pow(1 + annual, years)-1);
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String result = currency.format(answer);
+        
+        System.out.println("Mortgage: " + result);
+
+       
     }
 }
